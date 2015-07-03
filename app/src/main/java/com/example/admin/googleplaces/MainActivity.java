c2e1ee0d54c440fdd79b595d0bd005bcb486dcf3
Mikhail Valuyskiy <mvaluyskiy@weezlabs.com>
2015-07-03 18:20:37 +0300

    [google_map] Created route

--- app/src/main/java/com/example/admin/googleplaces/MainActivity.java
+++ app/src/main/java/com/example/admin/googleplaces/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.admin.googleplaces;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap map_; // Might be null if Google Play services APK is not available.
+    private static final LatLng SQUARE = new LatLng(47.208712, 38.936523);
+    private static final LatLng SQUARE_1 = new LatLng(47.208467, 38.936180);
+    private static final LatLng SQUARE_2 = new LatLng(47.209021, 38.934740);
+    private static final LatLng SQUARE_3 = new LatLng(47.209686, 38.935155);
+    private static final LatLng SQUARE_4 = new LatLng(47.209686, 38.935155);
+
+    private static final LatLng FAINA = new LatLng(47.210497, 38.933804);
+    private static final LatLng FAINA_1 = new LatLng(47.211109, 38.932806);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,19 +44,30 @@ public class MainActivity extends ActionBarActivity {
 
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
+        if (map_ == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            map_ = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
+            if (map_ != null) {
                 setUpMap();
+                addLines();
             }
         }
     }
 
+    private void addLines() {
+
+        map_.addPolyline((new PolylineOptions())
+                .add(SQUARE, SQUARE_1, SQUARE_2,SQUARE_3,SQUARE_4,FAINA,FAINA_1).width(5).color(Color.BLUE)
+                .geodesic(true));
+        // move camera to zoom on map
+        map_.moveCamera(CameraUpdateFactory.newLatLngZoom(SQUARE,
+                13));
+    }
+
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        map_.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
     @Override
